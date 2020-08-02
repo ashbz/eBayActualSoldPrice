b2cbf8f93fc695d35b60845dcf72c0191cff7367
ashbz <67267642+ashbz@users.noreply.github.com>
2020-08-02 14:49:38 +0300

    Add files via upload

---
+++ ebayprice.js
@@ -0,0 +1,42 @@
+function init(){
+
+	var soldElem = document.getElementById("prcIsum");
+	if (soldElem == null || soldElem == undefined) return;
+	
+	var html = document.body.parentElement.innerHTML;
+	
+	if (!html.includes("\"ended\":true")){
+		return;
+	}
+	var res = /taxExclusivePrice(.*?)",/.exec(html);
+	
+	if (res != null && res.length>1 && res[0].includes("taxExclusivePrice")){
+		
+		// check if best offer price is the same
+		var normPrice = /binPriceOnly(.*?),/.exec(html)
+		var offerPrice = /taxExclusivePrice(.*?),/.exec(html)		
+		
+		var finalPrice = res[1].replace("\"","").replace(":\"","");
+		var finalHtml = "";
+		
+		if (normPrice[1] == offerPrice[1]){
+			// we have offer, but same price
+			finalHtml = " - <span style='color: crimson'> Actual Price: N/A </span>";
+		}else{
+			finalHtml = " - <span style='color: green'> Actual Price: " + finalPrice + "  ⬇ </span>";
+		}
+		
+		if (!soldElem.innerHTML.includes("Actual Price")){
+			soldElem.innerHTML += finalHtml;
+		}
+		
+	}
+
+}
+
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded',afterDOMLoaded);
+} else {
+    init();
+}
+
